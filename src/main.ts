a3ec0d647eb0dda00bf9695d733bf75ec597ea84
PryAndrey <79829059+PryAndrey@users.noreply.github.com>
2021-07-24 05:00:45 +0300

    Delete main.ts

--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import {GAME} from './game'; 
-window.addEventListener('load', () => {
-    const game = new GAME();
-    game.start();
-    window.addEventListener('unload', () => game.stop());
-})
